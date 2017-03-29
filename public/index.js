5af4244dc9b33e3122d533cc6e7a0e10d9003230
Michael <mdurfey@me.com>
2017-03-28 23:20:26 -0700

    progress on data sorting method

--- public/index.js
+++ public/index.js
@@ -7,47 +7,29 @@ $(document).ready(function (){
         d = JSON.parse(d);
         // console.log(d);
         var html = "";
+        var newArray = [];
+
           d.forEach(function (value) { 
             if (d.indexOf(value) != 0) {
-              value.forEach(function (secondValue) {
-                var html = "";
-                // console.log(secondValue);
-                for (var i = 0; i <= value.length; i++) {
-                  console.log(secondValue[i]);
-                    if (i === 1) {
-                      html += "<div class = 'resultDiv' style = 'background-color: #F7F8F9; border-radius: 10px;'>";
-                      html += "<p class = 'resultText' style = 'color: black' id = 'resultText'>";
-                      html += secondValue[i];
-                      html += "</p>";
-                      html += "</div><br>";
-                    }
-
-                }
-                // console.log("secondValueIndex: " + d.indexOf(secondValue));
-                // console.log("secondValue: " + secondValue);
-
-                // if secondValue[secondKey]
-
+              var keys = Object.keys(value);
+              
+              d.find(function(element, index, array) {
+                  if (index === index) {
+                  newArray.push(element[index]);
+                  }
+                  console.log(newArray);
               })
-              // var keys = Object.keys(value);
-              // console.log(d[keys])
-              // if (value[keys] === value[1]) {
-                // html += "<div class = 'resultDiv' style = 'background-color: #F7F8F9; border-radius: 10px;'>";
-                // html += "<p class = 'resultText' style = 'color: black' id = 'resultText'>";
-                // html += values;
-                // html += "</p>"
-                // html += "</div><br>";
-              // }
-              // for (var i = 0; i <= value.length; i++) {
-
-
-              // }
-              // console.log(keys);
+              // var html = "";
+              // for (var i = 0; i <= value.length - 1; i ++) {
+              //     console.log(value[i])
+              //       array.push(value[i])
+              //        html += "<div class = 'resultDiv' style = 'background-color: #F7F8F9; border-radius: 10px;'>";
+              //         html += "<p class = 'resultText' style = 'color: black' id = 'resultText'>";
+              //         html += value[i];
+              //         html += "</p>";
+              //         html += "</div><br>";             
+              //   }
               }
-
-            // keys.forEach(function (value){ 
-              // var values = value[keys]
-            // })
             })
           $('#searchResultContainer').html(`${html}`);
           })
