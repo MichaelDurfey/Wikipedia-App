1dd935dd340993b308ce852bd91ead6329d055e1
Michael <mdurfey@me.com>
2017-03-30 14:13:49 -0700

    styling and animation complete

--- public/index.js
+++ public/index.js
@@ -7,7 +7,11 @@ $(document).ready(function (){
       }
     })
   })
+
     $('#submitButton').on('click', function (){ 
+      $('#searchBarContainer').fadeIn().css({top:400,position:'fixed'}).animate({top:8, marginTop:'10px'}, 1000);
+      $('#searchResultContainer').fadeIn().css({top:1000,position:'absolute'}).animate({top:136, marginTop: '10px'}, 1000);
+    
       var searchTerm = $('#userInput').val();
       $.get('/wikipediaSearch', {searchTerm}, function(d) { 
         d = JSON.parse(d);
@@ -26,13 +30,15 @@ $(document).ready(function (){
 
               let hasTitle = resultDiv.h1 !== '' && resultDiv.h1 !== undefined;
               let hasDesc  = resultDiv.description !== '' && resultDiv.description !== undefined && resultDiv.description.slice(searchTerm.length, -1) !== " may refer to";
-              // window.open('${resultDiv.url}');                            
+              let hasReferTo = resultDiv.description.slice(searchTerm.length, -1) === " may refer to";
+            
+
               if (hasTitle) {
                 html += `
                       <div
                         class='resultDiv'
-                        style='background-color: #F7F8F9; border-radius: 10px;'
-                        onclick=""
+                        style='background-color: #F7F8F9; border-radius: 0px;'
+                        onclick="window.open('${resultDiv.url}');"
                         >
                         <h1 class='resultText' style='color: black; font-size: 18px; font-weight: bold' id='resultText' >
                           ${resultDiv.h1}
